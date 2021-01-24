e2ce22c70529ae2508b274e27741ccf399f3ca84
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-23 22:23:15 -0500

    Serve static files and index at root

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 // Set port (process.env.PORT if deploying)
 const PORT = process.env.PORT || 8080;
 
+// Set static files
+app.use(express.static(__dirname + "/public"));
+
+// Server index.html at root route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 // Start server
 app.listen(PORT, function () {
   console.log(`App listening on: localhost:${PORT}`);
