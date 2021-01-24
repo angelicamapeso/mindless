8917498df14d2862f97c1a60401f2b4c84d50704
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-23 22:22:59 -0500

    Set up server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Server setup
+const express = require("express");
+const app = express();
+// Set port (process.env.PORT if deploying)
+const PORT = process.env.PORT || 8080;
+
+// Start server
+app.listen(PORT, function () {
+  console.log(`App listening on: localhost:${PORT}`);
+});
